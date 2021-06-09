696f1332e245f58e6a714b474d2c97531e79ad82
youssefelhaimer8 <youssefelhaimer8@gmail.com>
2021-06-09 02:16:31 +0100

    new project inra php mysql oop

---
+++ include/footer.php
@@ -0,0 +1,19 @@
+<!-- your script  -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" crossorigin="anonymous">
+</script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" crossorigin="anonymous"></script>
+
+<script src="<?php echo $site ?>/libs/js/wow.min.js"></script>
+<script src="<?php echo $site ?>/libs/js/fontewseme.js"></script>
+<script>
+new WOW().init();
+</script>
+<hr style="margin-top:60px;clear:both">
+<footer class="mt-6 p-4">
+    inra 2021
+</footer>
+<!--fin  home page -->
+</body>
+
+</html>
