9999a7a2bad994cfb9c7a16098998f9a264fdfb1
youssefelhaimer8 <youssefelhaimer8@gmail.com>
2021-06-09 02:15:54 +0100

    new project inra php mysql oop

---
+++ include/function.php
@@ -0,0 +1,9 @@
+<?php
+// if ($_SERVER['REQUEST_METHOD'] == 'POST' and isset($_POST['login'])) {
+
+//     $login->setInput($_POST['email'], $_POST['password']);
+// }
+//             // if($_SERVER['REQUEST_METHOD']=='POST' and isset($_POST['login'])){
+            //  $login->DisplayError();
+               
+            // }
